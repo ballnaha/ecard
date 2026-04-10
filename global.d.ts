32adbb2b6fd0ffd6656e8a19413b15deff36c9b5
ballnaha <ball_t1@hotmail.com>
2026-04-10 14:58:10 +0700

    update

--- global.d.ts
+++ global.d.ts
@@ -2,3 +2,9 @@ declare module '*.css' {
   const content: Record<string, string>;
   export default content;
 }
+
+declare module 'swiper/css';
+declare module 'swiper/css/navigation';
+declare module 'swiper/css/pagination';
+declare module 'swiper/css/effect-coverflow';
+declare module 'swiper/css/effect-creative';
